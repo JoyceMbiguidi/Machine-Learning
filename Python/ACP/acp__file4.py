1facc7f7d525a0ab6d068f3f3fb437363c0f1ffc
Joyce Mbiguidi <jmbiguidi@gmail.com>
2025-12-07 21:46:22 +0100

    suppression fichiers inutiles

--- Python/ACP/acp__file4.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-path = "https://raw.githubusercontent.com/JoyceMbiguidi/data/main/decathlon.csv"
-raw_df = pd.read_csv(path, sep = ";")
-
-print(raw_df.head())
-print(raw_df.dtypes)
-print(raw_df.select_dtypes(exclude=np.number).columns.tolist())
